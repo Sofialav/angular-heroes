21a1125997e0f8a390f5b690e255eeb5c0f31cbc
Sofialav <cemeteryvixen@gmail.com>
2020-05-01 16:21:53 +0200

    Add hero-lost with mock-data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from 'src/assets/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +8,12 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = { id: 1, name: 'Catherine' };
+  heroes = HEROES;
+  selectedHero: Hero;
   constructor() {}
 
   ngOnInit(): void {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
